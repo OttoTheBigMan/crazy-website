637b351e0ca63384668aadfa9f8defa674024346
RealBigMonk <105553762+RealBigMonk@users.noreply.github.com>
2023-12-10 11:48:25 +0100

    Fixed a base website with login and register

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { enhance } from "$app/forms";
+    import type { ActionData } from "./$types.js";
+
+    export let form: ActionData;
+    // export let data;
+</script>
+
+<!-- Enhance enhancear formuläret -->
+<main class="flex justify-center items-center h-screen w-screen">
+    <div class="flex flex-col items-center variant-glass-primary rounded-2xl p-5">
+        <h1 class="h1">Login</h1>
+        <form method="post" action="?/login" use:enhance class="flex flex-col items-center gap-5">
+            <label class="w-full">Email: <input type="email" name="email" class="input"/></label>
+            <label class="w-full">Password: <input type="password" name="password" class="input"/></label>
+
+            <button class="btn variant-ghost-secondary">Log In</button>
+            {#if form?.userName}
+                <span style="color: red;">{form.userName}</span>
+            {/if}
+        </form>
+        <span>Don't have an account? <a href="/register" class="text-blue-500 hover:underline">Register here</a></span>
+    </div>
+</main>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { enhance } from '$app/forms';
+    import type { ActionData, PageData } from './$types';
+    
+    export let form: ActionData;
+    // export let data: PageData;
+</script>
+<main class="flex justify-center items-center h-screen w-screen">
+    <div class="flex flex-col items-center variant-glass-primary rounded-2xl p-5">
+        <h1 class="h1 mb-2">Register account</h1>
+        <form action="?/register" method="post" use:enhance class="flex flex-col items-center gap-5">
+            <label class="w-full">E-mail: <input type="email" name="email" class="input"/></label>
+            <label class="w-full">Name: <input type="text" name="fullName" class="input"/></label>
+            <label class="w-full">Password: <input type="password" name="password" class="input"/></label>
+            <label class="w-full">Repeat Password: <input type="password" name="password-repeat" class="input"/></label>
+            <button class="btn variant-ghost-secondary">Submit</button>
+            {#if form?.msg}
+                <span style="color: red;">{form.msg}</span>
+            {/if}
+        </form>
+        <span>Already have an account? <a href="/login" class="text-blue-500 hover:underline">Log in here</a></span>
+    </div>
+</main>
